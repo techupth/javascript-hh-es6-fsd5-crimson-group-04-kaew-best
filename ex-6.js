679418ffd57edca6750b29b9fc2bf0910773c4d1
bestzaza789 <146432574+bestzaza789@users.noreply.github.com>
2023-12-13 13:39:24 +0700

    feat:finish all ex

--- ex-6.js
+++ ex-6.js
@@ -1,4 +1,7 @@
 let height = undefined;
 let result;
-
+function definedItem(height) {
+  return height ?? "Height is not defined";
+}
+result = definedItem(height);
 console.log(result); //  Result ควรจะได้ออกมาเป็น “Height is not defined”
